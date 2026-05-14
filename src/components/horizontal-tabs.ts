866981319d65588474b20107d35ffcaeeee2c8fd
ShawenHarker <harkershawen@gmail.com>
2026-05-14 08:53:01 +0200

    fix: horizontal tabs built to follow tina4js methods

--- src/components/horizontal-tabs.ts
+++ src/components/horizontal-tabs.ts
@@ -1,22 +1,32 @@
-import { html } from 'tina4js';
+import { html, Tina4Element } from 'tina4js';
 
-export const HorizontalTabs = (activePath: string) => {
-    const tabs = [
-        { icon: 'bi bi-pencil', href: '/new-entry', text: 'New Entry' },
-        { icon: 'bi bi-book', href: '/my-journal', text: 'My Journal' },
-    ];
+export class HorizontalTabs extends Tina4Element {
+    static props = {
+        activePath: String,
+    };
 
-    const tabClass = (href: string) =>
-        `nav-item ${activePath === href ? 'primary-bg' : ''} d-flex align-items-center rounded-top-2 px-2 me-2`.trim();
+    static shadow = false;
 
-    return html`
-        <ul class="nav nav-tabs text-primary-brand">
-            ${tabs.map(tab => html`
-                <li class="${tabClass(tab.href)}">
-                    <i class="${tab.icon}" style="margin-top: 4px;"></i>
-                    <a class="nav-link px-2 text-primary-brand" href="${tab.href}">${tab.text}</a>
-                </li>
-            `)}
-        </ul>
-    `;
+    render() {
+        const tabs = [
+            { icon: 'bi bi-pencil', href: '/new-entry', text: 'New Entry' },
+            { icon: 'bi bi-book', href: '/my-journal', text: 'My Journal' },
+        ];
+
+        const tabClass = (href: string) =>
+            `nav-item ${this.prop('activePath').value === href ? 'primary-bg' : ''} d-flex align-items-center rounded-top-2 px-2 me-2`.trim();
+
+        return html`
+            <ul class="nav nav-tabs text-primary-brand">
+                ${tabs.map(tab => html`
+                    <li class="${tabClass(tab.href)}">
+                        <i class="${tab.icon}" style="margin-top: 4px;"></i>
+                        <a class="nav-link px-2 text-primary-brand" href="${tab.href}">${tab.text}</a>
+                    </li>
+                `)}
+            </ul>
+        `;
+    }
 }
+
+customElements.define('horizontal-tabs', HorizontalTabs);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { html, route, navigate } from 'tina4js';
-import { HorizontalTabs } from '../components/horizontal-tabs';
+import  '../components/horizontal-tabs';
 import { NewEntry } from '../pages/newEntry';
 import { MyJournal } from '../pages/myJournal';
 
 const layout = (content: any, activePath: string) => html`
-    ${HorizontalTabs(activePath)}
+    <horizontal-tabs activePath="${activePath}"></horizontal-tabs>
     ${content}
 `;
 
